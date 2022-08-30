7a320582354f824cc936ba474c31919ba231664d
Adisthya Rahmadyan <adisthya.rahmadyan@gmail.com>
2022-08-30 11:33:38 +0700

    Props & State preparation.

--- src/app/App.js
+++ src/app/App.js
@@ -1,6 +1,8 @@
-import { Container } from 'react-bootstrap';
+import { Col, Container, Row } from 'react-bootstrap';
 import { BlogCard } from '../blog/BlogCard';
 import { GuestBook } from '../guest-book/GuestBook';
+import { Halo } from '../hello/Halo';
+import { Hello } from '../hello/Hello';
 import { Footer } from '../layout/Footer';
 import { Header } from '../layout/Header';
 
@@ -8,6 +10,14 @@ export function App() {
   return (
     <Container fluid>
       <Header />
+      <Row as="section" className="mx-5 my-3">
+        <Col sm="6">
+          <Hello />
+        </Col>
+        <Col sm="6">
+          <Halo />
+        </Col>
+      </Row>
       <BlogCard />
       <GuestBook />
       <Footer />
