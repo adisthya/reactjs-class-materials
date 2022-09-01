6828ff0a8800834e012f08f80cd67a2dd97720b7
Adisthya Rahmadyan <adisthya.rahmadyan@gmail.com>
2022-09-01 11:01:30 +0700

    Conditional rendering example.

--- src/counter/Counter.js
+++ src/counter/Counter.js
@@ -1,5 +1,5 @@
 import { Component } from "react";
-import { Button, Card, Col, Row } from "react-bootstrap";
+import { Badge, Button, Card, Col, Row } from "react-bootstrap";
 
 export class Counter extends Component {
   constructor(props) {
@@ -24,13 +24,27 @@ export class Counter extends Component {
     this.setState({ counter: this.state.counter - 1 });
   }
 
+  renderColoredNumber() {
+    const { counter } = this.state;
+
+    if (counter < 4) {
+      return <Badge pill bg="light" text="dark" className="px-3 fs-4">{ counter }</Badge>
+    } else if (counter >= 4 && counter < 7) {
+      return <Badge pill bg="warning" text="dark" className="px-3 fs-3">{ counter }</Badge>
+    } else if (counter >= 7 && counter <= 10) {
+      return <Badge pill bg="success" className="px-3 fs-2">{ counter }</Badge>
+    } else if (counter > 10) {
+      return <Badge pill bg="danger" className="px-3 fs-1">{ counter }</Badge>
+    }
+  }
+
   render() {
     return (
       <Row as="section" className="d-flex justify-content-center mx-5 my-5">
         <Col sm="2">
           <Card>
             <Card.Body>
-              <Card.Text className="fs-3 text-center">{ this.state.counter }</Card.Text>
+              <Card.Text className="fs-3 text-center">{ this.renderColoredNumber() }</Card.Text>
             </Card.Body>
             <Card.Footer className="d-flex justify-content-around">
                 <Button variant="primary" size="sm" onClick={ this.increment }
