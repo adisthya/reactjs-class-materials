d479a95a97f18ed9152d74486c33aeda28bf0201
Adisthya Rahmadyan <adisthya.rahmadyan@gmail.com>
2022-09-01 22:56:59 +0700

    controlled and uncontrolled component

--- src/guest-book/GuestForm.js
+++ src/guest-book/GuestForm.js
@@ -1,6 +1,5 @@
 import { Component, createRef } from "react";
 import { Card, Col, Row } from "react-bootstrap";
-
 export class GuestForm extends Component {
   constructor(props) {
     super(props);
@@ -8,6 +7,16 @@ export class GuestForm extends Component {
     this.state = {
       guestName: 'Jastin Bleber'
     };
+    this.guestPhoneRef = createRef();
+    this.handleGuestNameChange = this.handleGuestNameChange.bind(this);
+  }
+
+  handleGuestNameChange(event) {
+    this.setState({ guestName: event.target.value });
+  }
+
+  handleGuestPhoneChange() {
+    console.log('guest phone:', this.guestPhoneRef.current.value);
   }
 
   render() {
@@ -19,10 +28,13 @@ export class GuestForm extends Component {
               <Card.Title>Form Buku Tamu</Card.Title>
             </Card.Header>
             <Card.Body>
-              <input type="text" placeholder="Nama tamu" name="guestName" />
+              <input type="text" placeholder="Nama tamu" name="guestName"
+                value={this.state.guestName}
+                onChange={(e) => this.handleGuestNameChange(e)} />
             </Card.Body>
             <Card.Body>
-              <input type="text" placeholder="No. telpon tamu" name="guestPhone" />
+              <input type="text" placeholder="No. telpon tamu"
+                name="guestPhone" ref={this.guestPhoneRef} />
             </Card.Body>
           </Card>
         </Col>
