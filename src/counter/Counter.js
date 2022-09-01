c78600c3773cd4eb46783cffabd823fa9d2d652a
Adisthya Rahmadyan <adisthya.rahmadyan@gmail.com>
2022-09-01 12:07:18 +0700

    decrement code fix.

--- src/counter/Counter.js
+++ src/counter/Counter.js
@@ -5,27 +5,46 @@ export class Counter extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { counter: 0 };
+    this.state = { counters: [0, 0, 0, 0, 0] };
     this.increment = this.increment.bind(this);
     this.decrement = this.decrement.bind(this);
+    this.renderColoredNumber = this.renderColoredNumber.bind(this);
+    this.renderCounterCard = this.renderCounterCard.bind(this);
   }
 
-  increment() {
+  increment(index) {
     console.log('counter + 1');
     console.log('state:', this.state);
 
-    this.setState({ counter: this.state.counter + 1 });
+    this.setState({
+      counters: this.state.counters.map((counter, i) => {
+        if (index === i) {
+          return counter + 1;
+        } else {
+          return counter;
+        }
+      })
+    });
   }
 
-  decrement() {
+  decrement(index) {
     console.log('counter - 1');
     console.log('state:', this.state);
 
-    this.setState({ counter: this.state.counter - 1 });
+    this.setState({
+      counters: this.state.counters.map((counter, i) => {
+        if (index === i) {
+          return counter - 1;
+        } else {
+          return counter;
+        }
+      })
+    });
   }
 
-  renderColoredNumber() {
-    const { counter } = this.state;
+  renderColoredNumber(index) {
+    const { counters } = this.state;
+    const counter = counters[index];
 
     if (counter < 4) {
       return <Badge pill bg="light" text="dark" className="px-3 fs-4">{ counter }</Badge>
@@ -38,22 +57,34 @@ export class Counter extends Component {
     }
   }
 
+  renderCounterCard(index) {
+    return (
+      <Col sm="2" key={index}>
+        <Card>
+          <Card.Body>
+            <Card.Text className="fs-3 text-center">{ this.renderColoredNumber(index) }</Card.Text>
+          </Card.Body>
+          <Card.Footer className="d-flex justify-content-around">
+              <Button variant="primary" size="sm" onClick={ () => this.increment(index) }
+                className="text-bold fs-5 px-5">+</Button>
+              <Button variant="primary" size="sm" onClick={ () => this.decrement(index) }
+                className="text-bold fs-5 ms-1 px-5">-</Button>
+          </Card.Footer>
+        </Card>
+      </Col>
+    );
+  }
+
   render() {
+    const { counters } = this.state;
+
     return (
       <Row as="section" className="d-flex justify-content-center mx-5 my-5">
-        <Col sm="2">
-          <Card>
-            <Card.Body>
-              <Card.Text className="fs-3 text-center">{ this.renderColoredNumber() }</Card.Text>
-            </Card.Body>
-            <Card.Footer className="d-flex justify-content-around">
-                <Button variant="primary" size="sm" onClick={ this.increment }
-                  className="text-bold fs-5 px-5">+</Button>
-                <Button variant="primary" size="sm" onClick={ this.decrement }
-                  className="text-bold fs-5 ms-1 px-5">-</Button>
-            </Card.Footer>
-          </Card>
-        </Col>
+        {
+          counters.map((counter, index) => {
+            return this.renderCounterCard(index);
+          })
+        }
       </Row>
     );
   }
