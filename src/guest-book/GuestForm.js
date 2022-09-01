559f99ed90b62d2bc225f71ff0816e827223031e
Adisthya Rahmadyan <adisthya.rahmadyan@gmail.com>
2022-09-01 23:42:58 +0700

    Guest Book form completed.

--- src/guest-book/GuestForm.js
+++ src/guest-book/GuestForm.js
@@ -1,40 +1,70 @@
-import { Component, createRef } from "react";
-import { Card, Col, Row } from "react-bootstrap";
+import { Component } from "react";
+import { Button, Card, Col, Form, Row } from "react-bootstrap";
 export class GuestForm extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      guestName: 'Jastin Bleber'
+      value: {
+        guestName: '',
+        guestEmail: '',
+        guestPhone: '',
+        guestAddress: ''
+      }
     };
-    this.guestPhoneRef = createRef();
-    this.handleGuestNameChange = this.handleGuestNameChange.bind(this);
+    this.handleValueChange = this.handleValueChange.bind(this);
+    this.handleFormSubmit = this.handleFormSubmit.bind(this);
   }
 
-  handleGuestNameChange(event) {
-    this.setState({ guestName: event.target.value });
+  handleValueChange(field, event) {
+    const { target: { value } } = event;
+    const fields = Object.keys(this.state.value);
+
+    if (fields.includes(field)) {
+      this.setState({
+        value: { ...this.state.value, [field]: value }
+      });
+    }
   }
 
-  handleGuestPhoneChange() {
-    console.log('guest phone:', this.guestPhoneRef.current.value);
+  handleFormSubmit(e) {
+    e.preventDefault();
+    console.log('value', this.state.value);
   }
 
   render() {
+    const { value } = this.state;
+
     return (
       <Row as="section" className="d-flex justify-content-center">
         <Col sm="8">
-          <Card className="mb-5 py-3">
+          <Card className="mb-5">
             <Card.Header>
               <Card.Title>Form Buku Tamu</Card.Title>
             </Card.Header>
             <Card.Body>
-              <input type="text" placeholder="Nama tamu" name="guestName"
-                value={this.state.guestName}
-                onChange={(e) => this.handleGuestNameChange(e)} />
-            </Card.Body>
-            <Card.Body>
-              <input type="text" placeholder="No. telpon tamu"
-                name="guestPhone" ref={this.guestPhoneRef} />
+              <Form onSubmit={this.handleFormSubmit}>
+                <Form.Group className="mb-3">
+                  <Form.Control placeholder="Nama Tamu" value={value.guestName}
+                    onChange={(e) => this.handleValueChange('guestName', e)} />
+                </Form.Group>
+                <Form.Group className="mb-3">
+                  <Form.Control placeholder="Email Tamu" value={value.guestEmail}
+                    onChange={(e) => this.handleValueChange('guestEmail', e)} />
+                </Form.Group>
+                <Form.Group className="mb-3">
+                  <Form.Control placeholder="Telpon Tamu" value={value.guestPhone}
+                    onChange={(e) => this.handleValueChange('guestPhone', e)} />
+                </Form.Group>
+                <Form.Group className="mb-3">
+                  <Form.Control placeholder="Alamat Tamu" as="textarea" value={value.guestAddress}
+                    onChange={(e) => this.handleValueChange('guestAddress', e)} />
+                </Form.Group>
+                <Form.Group className="d-flex justify-content-end">
+                  <Button variant="primary" type="submit" className="me-2">Simpan</Button>
+                  <Button variant="light" type="reset">Reset</Button>
+                </Form.Group>
+              </Form>
             </Card.Body>
           </Card>
         </Col>
