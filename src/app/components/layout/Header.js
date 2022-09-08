0baf883fba88c11d23c8c92b4eb2f75159376873
Adisthya Rahmadyan <adisthya.rahmadyan@gmail.com>
2022-09-08 23:39:18 +0700

    style update.

--- src/app/components/layout/Header.js
+++ src/app/components/layout/Header.js
@@ -2,9 +2,9 @@ import { Col, Container, Navbar, Row } from "react-bootstrap";
 
 export function Header() {
   return (
-    <Row as="header">
-      <Col className="p-0 mb-2">
-        <Navbar bg="dark" variant="dark" expand="sm" className="shadow-lg border-bottom border-3 border-danger">
+    <Row as="header" className="border-bottom border-3 border-danger mb-5 shadow-lg">
+      <Col xs="12" className="p-0">
+        <Navbar bg="dark" variant="dark">
           <Container fluid>
             <Navbar.Brand href="#">
               <h1>My Blog</h1>
