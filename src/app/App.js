7f813f31503545400e251ae6efd60abd5978da30
Adisthya Rahmadyan <adisthya.rahmadyan@gmail.com>
2022-09-09 01:14:07 +0700

    done demo render props.

--- src/app/App.js
+++ src/app/App.js
@@ -2,12 +2,14 @@ import { Container } from 'react-bootstrap';
 import { DemoHoc } from './components/hoc/DemoHoc';
 import { Footer } from './components/layout/Footer';
 import { Header } from './components/layout/Header';
+import { DemoRenderProps } from './components/render-props/DemoRenderProps';
 
 export function App() {
   return (
     <Container fluid>
       <Header />
       <DemoHoc />
+      <DemoRenderProps />
       <Footer />
     </Container>
   );
